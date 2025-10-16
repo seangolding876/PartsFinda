736255d61888d5339a36d330cc067237c57ada23
Adnan Muhammad Shafi <56834847+adnan-qnkhan@users.noreply.github.com>
2025-10-16 18:54:20 +0500

    eco syerm (ak)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,21 +6,14 @@ module.exports = {
       script: 'npm',
       args: 'start',
       cwd: '/var/www/partsfinda',
-      instances: 'max',
+      instances: 'max', 
       exec_mode: 'cluster',
-      env: { NODE_ENV: 'production', PORT: 3000 }
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      }
     },
 
-    // Worker
-    {
-      name: 'partsfinda-worker', 
-      script: './dist-worker/start_worker.js',
-      cwd: '/var/www/partsfinda',
-      instances: 1,
-      exec_mode: 'fork'
-    },
-    
-    // Socket Server (from your existing /opt/socket-server)
     {
       name: 'partsfinda-socket',
       script: 'index.js', 
@@ -30,7 +23,16 @@ module.exports = {
         NODE_ENV: 'production',
         SOCKET_PORT: 3001,
         JWT_SECRET: 'r9fQqsPeEJP6QbbN82RytCYqt1Dw1cc82AR66IibocE'
-      }
+  }
+},
+
+    // Simple Worker
+    {
+      name: 'partsfinda-worker',
+      script: './dist-worker/start_worker.js', // ✅ YEH FILE
+      cwd: '/var/www/partsfinda', 
+      instances: 1,
+      exec_mode: 'fork'
     }
   ]
 };
