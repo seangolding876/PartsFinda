91b4279e00b010ef04f05e6bb332e3814e1dbc29
seangolding876 <seangolding@hotmail.com>
2025-10-16 21:23:51 +0500

    ecosystem (ak)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -28,16 +28,17 @@ module.exports = {
     },
     
     // Socket Server
+     // ✅ Socket Server - FIXED
     {
       name: 'partsfinda-socket',
       script: 'index.js',
       cwd: '/var/www/partsfinda/opt/socket-server',
       instances: 1,
+      exec_mode: 'fork', // ✅ YEH ADD KARO
       env: {
         NODE_ENV: 'production',
         SOCKET_PORT: 3001,
         JWT_SECRET: 'r9fQqsPeEJP6QbbN82RytCYqt1Dw1cc82AR66IibocE'
       }
-    }
   ]
 };
