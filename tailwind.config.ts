16bb311078400e8ae0909a63e0f35eddd6ae9146
Adnan Muhammad Shafi <56834847+adnan-qnkhan@users.noreply.github.com>
2025-10-23 13:36:30 +0500

    update (ak)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,13 @@
-// tailwind.config.js
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+export default {
   content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+} satisfies Config;
