b31898cbd92be32a70af28689d5bb306a0d0d45b
seangolding876 <seangolding@gmail.com>
2026-02-03 15:47:15 +0500

    aaaa

--- next.config.js
+++ next.config.js
@@ -47,19 +47,19 @@ const nextConfig = {
   poweredByHeader: false,
 
   // ✅ Add this to handle API routes properly
-  // async headers() {
-  //   return [
-  //     {
-  //       source: '/api/:path*',
-  //       headers: [
-  //         { key: 'Access-Control-Allow-Credentials', value: 'true' },
-  //         { key: 'Access-Control-Allow-Origin', value: '*' },
-  //         { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
-  //         { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
-  //       ]
-  //     }
-  //   ]
-  // }
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
